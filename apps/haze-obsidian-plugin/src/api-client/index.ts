281de9cd710932da9d811da19da85d178bee4ad3
Коржиков Дмитрий <92763874+NordCoder@users.noreply.github.com>
2026-07-07 13:58:23 +0300

    Export Obsidian API client foundation

---
+++ apps/haze-obsidian-plugin/src/api-client/index.ts
@@ -0,0 +1,45 @@
+export { HazeSyncApiClient } from "./client";
+export type { HazeSyncApiClientOptions, HttpTransport } from "./client";
+export {
+  ApiClientError,
+  createHttpError,
+  createInvalidResponseError,
+  createOfflineError,
+  mapHttpStatusToCategory,
+} from "./errors";
+export type { ApiErrorCategory, SafeApiErrorSummary } from "./errors";
+export {
+  apiClientConfigFromSettings,
+  assertConfiguredServer,
+  assertSameOrigin,
+  buildApiHeaders,
+  buildApiUrl,
+  buildFilePathUrl,
+  encodeVaultPath,
+} from "./request";
+export type { ApiClientConfig, RequestHeadersInput } from "./request";
+export type {
+  ChangeDto,
+  ChangeKind,
+  ChangesRequest,
+  ChangesResponseDto,
+  ConflictDto,
+  ConflictId,
+  ConflictListRequest,
+  ConflictResolutionAction,
+  ConflictsResponseDto,
+  ContentHash,
+  DeleteFileRequest,
+  DeleteFileResponseDto,
+  ErrorResponseDto,
+  FileDownload,
+  FileMetadataDto,
+  OperationId,
+  PutFileRequest,
+  PutFileResponseDto,
+  ResolveConflictRequest,
+  ResolveConflictResponseDto,
+  RevisionId,
+  ServerInfoDto,
+  VaultPath,
+} from "./types";
